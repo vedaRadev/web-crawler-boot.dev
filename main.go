8ab05ffb5543185600e684a0875b9618be5e71bc
vedaRadev <ryancarmstrong97@gmail.com>
2025-04-11 14:00:49 -0400

    function for getting html

--- main.go
+++ main.go
@@ -3,7 +3,27 @@ package main
 import (
 	"os"
 	"fmt"
+	"io"
+	"net/http"
+	"strings"
 )
+
+func getHTML(rawURL string) (string, error) {
+	resp, err := http.Get(rawURL)
+	if err != nil { return "", err }
+	if resp.StatusCode >= 400 && resp.StatusCode <= 499 {
+		return "", fmt.Errorf("400 status: %v", resp.StatusCode)
+	}
+	contentType := resp.Header.Get("content-type")
+	if strings.Contains("txt/html", contentType) {
+		return "", fmt.Errorf("invalid content type: %v", contentType)
+	}
+
+	html, err := io.ReadAll(resp.Body)
+	if err != nil { return "", err }
+
+	return string(html), nil
+}
 
 func main() {
 	args := os.Args[1:]
@@ -18,4 +38,10 @@ func main() {
 
 	baseURL := args[0]
 	fmt.Printf("starting crawl of: %v\n", baseURL)
+	html, err := getHTML(baseURL)
+	if err != nil {
+		fmt.Printf("failed to get html: %v\n", err)
+	} else {
+		fmt.Println(html)
+	}
 }
